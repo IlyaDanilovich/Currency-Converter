6e8e430ede0c4d1b99c6735cec7432485e57437f
Ilya Danilovich <ilyadan98@gmail.com>
2019-11-25 23:15:25 +0300

    Add core business logic

---
+++ Converter/Converter/ConverterCore.cs
@@ -0,0 +1,10 @@
+﻿namespace Converter
+{
+    class ConverterCore
+    {
+        public double Convert(double amount, double fromRate, double toRate)
+        {
+            return fromRate / toRate * amount;
+        }
+    }
+}
--- Converter/Converter/MainWindow.xaml.cs
+++ Converter/Converter/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Threading;
+using Newtonsoft.Json.Linq;
 
 namespace Converter
 {
@@ -23,6 +16,101 @@ namespace Converter
         public MainWindow()
         {
             InitializeComponent();
+            InitializeConnection();
+
+            fromCurrency.SelectedIndex = 0;
+            toCurrency.SelectedIndex = 1;
+
+            convertButton.IsEnabled = false;
+        }
+
+        private Dictionary<string, double> Currencies { get; set; }
+
+        private void WaitingConnection()
+        {
+            while (!NetworkInterface.GetIsNetworkAvailable())
+            {
+            }
+
+            Currencies = new Dictionary<string, double>
+            {
+                {"USD", GetCurrRate(145)},
+                {"EUR", GetCurrRate(292)},
+                {"RUB", GetCurrRate(298)},
+                {"BYN", 1}
+            };
+
+            fromAmount.Dispatcher.Invoke(TextBoxReady);
+        }
+
+        private void TextBoxReady()
+        {
+            fromAmount.IsReadOnly = false;
+            messageBox.Text = $"EUR: {Currencies["EUR"]}\r\nUSD: {Currencies["USD"]}\r\nRUB: {Currencies["RUB"]}\n";
+        }
+
+        private void InitializeConnection()
+        {
+            messageBox.Text = @"Подключение к интернету...";
+            fromAmount.IsReadOnly = true;
+
+            var waitingConnection = new Thread(WaitingConnection);
+
+            waitingConnection.Start();
+        }
+
+        private double GetCurrRate(int id)
+        {
+            var rawResponse = DownloadPage($"http://www.nbrb.by/API/ExRates/Rates/{id}");
+
+            var response = JObject.Parse(rawResponse);
+
+            return (double)response.SelectToken("Cur_OfficialRate") / (double)response.SelectToken("Cur_Scale");
+        }
+
+        private static string DownloadPage(string url)
+        {
+            string response;
+
+            using (var wc = new WebClient())
+            {
+                response = wc.DownloadString(url);
+            }
+
+            return response;
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            var amount = Convert.ToDouble(fromAmount.Text);
+
+            var converter = new ConverterCore();
+
+            var convertedAmount = converter.Convert(
+                amount,
+                Currencies[fromCurrency.Text],
+                Currencies[toCurrency.Text]
+            );
+
+            double roundedAmount = Math.Round(convertedAmount, 2);
+
+            toAmount.Text = roundedAmount.ToString();
+        }
+
+        private void FromAmount_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            try
+            {
+                Convert.ToDouble(fromAmount.Text);
+            }
+            catch (FormatException)
+            {
+                convertButton.IsEnabled = false;
+
+                return;
+            }
+
+            convertButton.IsEnabled = true;
         }
     }
 }
